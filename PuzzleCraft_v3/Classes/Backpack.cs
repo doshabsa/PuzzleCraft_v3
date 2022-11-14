a71cf19613725846b60a50a43d2b4186c500010d
Shabs <85132269+doshabsa@users.noreply.github.com>
2022-11-13 17:30:33 -0700

    Backpack now working but could use better placement

--- PuzzleCraft_v3/Classes/Backpack.cs
+++ PuzzleCraft_v3/Classes/Backpack.cs
@@ -17,19 +17,14 @@ namespace PuzzleCraft_v3
         private static Panel ClosedBag;
         private static Label lblOpenBag;
         private static Label lblCloseBag;
-        private static Size lblOpenBagSize = new Size(380, 210);
+        private static Size OpenBagSize = new Size(380, 210);
         private static Size ClosedSize = new Size(110, 30);
 
         public Backpack()
         {
             this.Font = new Font("Arial", 12);
-            this.Size = lblOpenBagSize;
-            this.Location = new Point(3, 3);
-            this.BackColor = Color.Transparent;
-
             SetupOpenBag();
             SetupCloseBag();
-            BaseChar.MainForm?.Controls.Add(this);
         }
 
         private void ToggleBag_Click(object? sender, EventArgs e)
@@ -37,22 +32,16 @@ namespace PuzzleCraft_v3
             if (lblOpenBag.Visible)
             {
                 BaseChar.PlayerTimer.Start();
-                this.Size = ClosedSize;
                 OpenedBag.Visible = false;
-                lblOpenBag.Visible = false;
                 ClosedBag.Visible = true;
-                lblCloseBag.Visible = true;
-                lblCloseBag.BringToFront();
+                ClosedBag.BringToFront();
             }
             else
             {
                 BaseChar.PlayerTimer.Stop();
-                this.Size = lblOpenBagSize;
                 OpenedBag.Visible = true;
-                lblOpenBag.Visible = true;
                 ClosedBag.Visible = false;
-                lblCloseBag.Visible = false;
-                lblOpenBag.BringToFront();
+                OpenedBag.BringToFront();
             }
         }
 
@@ -64,10 +53,9 @@ namespace PuzzleCraft_v3
                 Name = "pnlBagClose",
                 BackColor = Color.OldLace,
                 BorderStyle = BorderStyle.FixedSingle,
-                Anchor = AnchorStyles.Left | AnchorStyles.Top//,
-                //Dock = DockStyle.Fill
+                Anchor = AnchorStyles.Left | AnchorStyles.Bottom
             };
-            this.Controls.Add(ClosedBag);
+            BaseChar.MainForm.Controls.Add(ClosedBag);
 
             lblCloseBag = new();
             lblCloseBag.Location = new Point(ClosedBag.Width / 2 - lblCloseBag.Width / 2, 4);
@@ -77,24 +65,22 @@ namespace PuzzleCraft_v3
             lblCloseBag.Font = new Font(this.Font, FontStyle.Bold);
             lblCloseBag.ForeColor = Color.DarkGoldenrod;
             lblCloseBag.TextAlign = ContentAlignment.MiddleCenter;
-            this.Controls.Add(lblCloseBag);
+            ClosedBag.Controls.Add(lblCloseBag);
             lblCloseBag.Click += ToggleBag_Click;
-            lblCloseBag.BringToFront();
         }
 
         private void SetupOpenBag()
         {
             OpenedBag = new()
             {
-                Size = new Size(this.Width - 6, this.Height - 6),
+                Size = OpenBagSize,
                 Name = "pnlBagOpen",
                 BackColor = Color.OldLace,
                 BorderStyle = BorderStyle.FixedSingle,
                 Visible = false,
-                Anchor = AnchorStyles.Left | AnchorStyles.Top//,
-                //Dock = DockStyle.Fill
+                Anchor = AnchorStyles.Left | AnchorStyles.Bottom
             };
-            this.Controls.Add(OpenedBag);
+            BaseChar.MainForm.Controls.Add(OpenedBag);
 
             lblOpenBag = new();
             lblOpenBag.Location = new Point(OpenedBag.Width / 2 - lblOpenBag.Width / 2, 4);
@@ -104,10 +90,8 @@ namespace PuzzleCraft_v3
             lblOpenBag.Font = new Font(this.Font, FontStyle.Bold);
             lblOpenBag.ForeColor = Color.DarkGoldenrod;
             lblOpenBag.TextAlign = ContentAlignment.MiddleCenter;
-            lblOpenBag.Visible = false;
-            this.Controls.Add(lblOpenBag);
+            OpenedBag.Controls.Add(lblOpenBag);
             lblOpenBag.Click += ToggleBag_Click;
-            lblOpenBag.BringToFront();
         }
 
         public static void GameReset()
